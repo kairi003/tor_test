5549fcad1fb605ac98d41068ef689450e67a2cf6
kairi003 <kairi.satellite@gmail.com>
2021-01-16 20:22:43 +0900

    [update]

--- app.py
+++ app.py
@@ -22,8 +22,8 @@ def tor():
     return res.text
 
 
-@app.route('/niji-gazo/')
-def tor():
+@app.route('/test/')
+def test():
     headers = {
         'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 12_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/12.0 Mobile/15E148 Safari/604.1'
     }
