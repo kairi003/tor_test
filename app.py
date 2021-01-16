f0c31525b9ddc069d3282ef701753b533e65e064
kairi003 <kairi.satellite@gmail.com>
2021-01-16 20:32:06 +0900

    [update]

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import requests
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
@@ -24,15 +24,15 @@ def tor():
 
 @app.route('/test/')
 def test():
+    url = request.args.get('url', 'https://ipinfo.io')
     headers = {
-        'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 12_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/12.0 Mobile/15E148 Safari/604.1'
+        'User-Agent': request.headers.get('User-Agent')
     }
     proxies = {
         'http': 'socks5://127.0.0.1:9050',
         'https': 'socks5://127.0.0.1:9050'
     }
-    res = requests.get('http://niji-gazo.com',
-                       headers=headers, proxies=proxies)
+    res = requests.get(url, headers=headers, proxies=proxies)
     return res.text
 
 
